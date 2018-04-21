d6c1917e6484e70cd726bd3d2c9ad40af4c9d373
Michael J. Cusack <whoneedszzz@gmail.com>
2018-04-20 21:08:18 -0500

    Add support for clj-time

--- rsvp-tracker-api/project.clj
+++ rsvp-tracker-api/project.clj
@@ -4,6 +4,7 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [cheshire "5.8.0"]
+                 [clj-time "0.14.3"]
                  [compojure "1.6.1"]
                  [ring/ring-core "1.6.3"]
                  [ring/ring-defaults "0.3.1"]
