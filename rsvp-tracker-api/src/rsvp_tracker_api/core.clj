742bc32b427948449b6c7fc4856ed5301dbca645
Michael J. Cusack <whoneedszzz@gmail.com>
2018-04-21 02:35:00 -0500

    Implement updating an event
    Add PUT for events endpoint with supplied id. If the id is invalid the response body will be "null".

--- rsvp-tracker-api/src/rsvp_tracker_api/core.clj
+++ rsvp-tracker-api/src/rsvp_tracker_api/core.clj
@@ -2,11 +2,11 @@
   (:gen-class)
   (:require
     [cheshire.core :refer [generate-string]]
-    [compojure.core :refer [defroutes GET POST]]
+    [compojure.core :refer [defroutes GET POST PUT]]
     [ring.middleware.cors :refer [wrap-cors]]
     [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
     [ring.util.response :refer [content-type response status]]
-    [rsvp-tracker-api.event :refer [create-event get-event get-events]]))
+    [rsvp-tracker-api.event :refer [create-event get-event get-events update-event]]))
 
 (def base-path "/api/v1")
 
@@ -30,6 +30,9 @@
         (content-type "application/json")))
   (GET (str base-path "/events/:id") [id]
     (-> (response (get-event id))
+        (content-type "application/json")))
+  (PUT (str base-path "/events/:id") [id :as request]
+    (-> (response (update-event id request))
         (content-type "application/json"))))
 
 (def handler
