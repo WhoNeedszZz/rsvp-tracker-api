b0056d91fb44969d3021e395a1589265e344a837
Michael J. Cusack <whoneedszzz@gmail.com>
2018-04-20 22:38:51 -0500

    Test: View events
    Test viewing all events when there are events and when there aren't.

--- rsvp-tracker-api/test/rsvp_tracker_api/core_test.clj
+++ rsvp-tracker-api/test/rsvp_tracker_api/core_test.clj
@@ -18,7 +18,22 @@
                     "accepted" []
                     "declined" []})
 
+(def event2 {"title" "Test Event 2"
+             "location" "Scott, LA"
+             "dateStart" "2017-11-27T23:00:00.000Z"
+             "dateEnd" "2017-11-28T02:00:00.000Z"
+             "description" "Another round"
+             "organizers" ["john@foo.com"]
+             "invited" ["adam@foo.com" "sarah@foo.com"]
+             "tentative" []
+             "accepted" []
+             "declined" []})
+
 (def create-error (generate-string {:error "Failed to create event"}))
+
+(def events-empty (generate-string {:message "There are currently no events"}))
+
+(def view-error (generate-string {:error "Failed to retrieve events"}))
 
 (deftest api-base
   (testing "Initial bogus JSON data response from /"
@@ -26,6 +41,13 @@
            {:status 200
             :headers {"Content-Type" "application/json"}
             :body (generate-string {:foo :bar})}))))
+
+(deftest view-all-events-empty
+  (testing "View all events when events is empty"
+    (is (= (handler (request :get events-endpoint))
+           {:status 200
+            :headers {"Content-Type" "application/json"}
+            :body (generate-string [])}))))
 
 (deftest create-event-valid-data
   (testing "Create an event at /api/v1/events"
@@ -42,3 +64,10 @@
            {:status 400
             :headers {"Content-Type" "application/json"}
             :body create-error}))))
+
+(deftest view-all-events
+  (testing "View all events when there is an event"
+    (is (not= (handler (request :get events-endpoint))
+           {:status 200
+            :headers {"Content-Type" "application/json"}
+            :body view-error}))))
