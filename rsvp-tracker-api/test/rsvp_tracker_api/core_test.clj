b25138a81b08addcde2652f0cb98b7164a7be0c3
Michael J. Cusack <whoneedszzz@gmail.com>
2018-04-20 21:10:49 -0500

    Refactor ring.mock.request to use refer instead of as

--- rsvp-tracker-api/test/rsvp_tracker_api/core_test.clj
+++ rsvp-tracker-api/test/rsvp_tracker_api/core_test.clj
@@ -1,12 +1,12 @@
 (ns rsvp-tracker-api.core-test
   (:require
     [clojure.test :refer [deftest is testing]]
-    [ring.mock.request :as mock]
+    [ring.mock.request :refer [json-body request]]
     [rsvp-tracker-api.core :refer [handler]]))
 
 (deftest api-base
   (testing "Initial bogus JSON data response from /"
-    (is (= (handler (mock/request :get "/"))
+    (is (= (handler (request :get "/"))
            {:status 200
             :headers {"Content-Type" "application/json"}
             :body "{\"foo\":\"bar\"}"}))))
